f4e9d949f7ec8b2b11d6198901960487bc3685d7
valencra <valencra@mcmaster.ca>
2017-12-26 15:11:47 -0600

    Implemented service classes

---
+++ src/main/java/com/valencra/instateam/service/ProjectServiceImpl.java
@@ -0,0 +1,35 @@
+package com.valencra.instateam.service;
+
+import com.valencra.instateam.dao.ProjectDao;
+import com.valencra.instateam.model.Project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+  @Autowired
+  private ProjectDao projectDao;
+
+  @Override
+  public List<Project> findAll() {
+    return projectDao.findAll();
+  }
+
+  @Override
+  public Project findById(Long id) {
+    return projectDao.findById(id);
+  }
+
+  @Override
+  public void save(Project project) {
+    projectDao.save(project);
+  }
+
+  @Override
+  public void delete(Project project) {
+    projectDao.delete(project);
+  }
+}
---
+++ src/main/java/com/valencra/instateam/service/RoleServiceImpl.java
@@ -0,0 +1,35 @@
+package com.valencra.instateam.service;
+
+import com.valencra.instateam.dao.RoleDao;
+import com.valencra.instateam.model.Role;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+  @Autowired
+  private RoleDao roleDao;
+
+  @Override
+  public List<Role> findAll() {
+    return roleDao.findAll();
+  }
+
+  @Override
+  public Role findById(Long id) {
+    return roleDao.findById(id);
+  }
+
+  @Override
+  public void save(Role role) {
+    roleDao.save(role);
+  }
+
+  @Override
+  public void delete(Role role) {
+    roleDao.delete(role);
+  }
+}
