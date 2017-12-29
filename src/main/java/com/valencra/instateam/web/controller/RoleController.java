9771c9f3822a8b4926635092c91c0c305685a558
valencra <valencra@mcmaster.ca>
2017-12-29 14:53:24 -0600

    Added controller methods for editing existing roles

--- src/main/java/com/valencra/instateam/web/controller/RoleController.java
+++ src/main/java/com/valencra/instateam/web/controller/RoleController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -27,9 +30,10 @@ public class RoleController {
     return "role/index";
   }
 
-  // New role form
+  // Add new role form
   @GetMapping("/roles/add")
   public String newRoleForm(Model model) {
+    // If no role object exists, instantiate a new instance
     if(!model.containsAttribute("role")) {
       model.addAttribute("role", new Role());
     }
@@ -42,6 +46,7 @@ public class RoleController {
   // Add new role
   @PostMapping(value = "/roles")
   public String addNewRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
+    // If result has errors, add results and current role object to the model, then redirect to form
     if(result.hasErrors()) {
       redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.role", result);
       redirectAttributes.addFlashAttribute("role", role);
@@ -49,6 +54,33 @@ public class RoleController {
     }
     roleService.save(role);
     redirectAttributes.addFlashAttribute("flash", new FlashMessage("Role successfully added!", FlashMessage.Status.SUCCESS));
+    return "redirect:/roles";
+  }
+
+  // Edit existing role form
+  @GetMapping("/roles/{id}/edit")
+  public String editRoleForm(@PathVariable Long id, Model model) {
+    // If no role object exists, get role with given id
+    if(!model.containsAttribute("role")) {
+      model.addAttribute("role", roleService.findById(id));
+    }
+    model.addAttribute("action", String.format("/roles/%d", id));
+    model.addAttribute("heading","Edit Role");
+    model.addAttribute("submit","Update");
+    return "role/form";
+  }
+
+  // Edit existing role
+  @PutMapping(value = "/roles/{id}")
+  public String editExistingRole(@Valid Role role, BindingResult result, RedirectAttributes redirectAttributes) {
+    // If result has errors, add results and current role object to the model, then redirect to form
+    if(result.hasErrors()) {
+      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.role", result);
+      redirectAttributes.addFlashAttribute("role", role);
+      return "redirect:/roles/{id}/edit";
+    }
+    roleService.save(role);
+    redirectAttributes.addFlashAttribute("flash", new FlashMessage("Role successfully edited!", FlashMessage.Status.SUCCESS));
     return "redirect:/roles";
   }
 }
