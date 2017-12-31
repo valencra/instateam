28888af9aba833a1c8ef203927bf787d7cf7be73
valencra <valencra@mcmaster.ca>
2017-12-31 09:21:25 -0600

    Added project controller and method for getting all projects

---
+++ src/main/java/com/valencra/instateam/web/controller/ProjectController.java
@@ -0,0 +1,32 @@
+package com.valencra.instateam.web.controller;
+
+import com.valencra.instateam.model.Project;
+import com.valencra.instateam.service.CollaboratorService;
+import com.valencra.instateam.service.ProjectService;
+import com.valencra.instateam.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProjectController {
+  @Autowired
+  private ProjectService projectService;
+
+  @Autowired
+  private CollaboratorService collaboratorService;
+
+  @Autowired
+  private RoleService roleService;
+
+  @GetMapping("/")
+  public String getProjects(Model model) {
+    List<Project> projects = projectService.findAll();
+    model.addAttribute("projects", projects);
+    return "project/index";
+  }
+
+}
