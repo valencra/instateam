03cfec47cdf10cde70ac13362dafb911a3db3b29
valencra <valencra@mcmaster.ca>
2018-01-06 20:32:30 -0500

    Role collaborators now eagerly loaded

--- src/main/java/com/valencra/instateam/model/Role.java
+++ src/main/java/com/valencra/instateam/model/Role.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,7 +23,7 @@ public class Role {
   @NotBlank(message = "Role name must not be blank")
   private String name;
 
-  @OneToMany(mappedBy = "role")
+  @OneToMany(mappedBy = "role", fetch = FetchType.EAGER)
   private List<Collaborator> collaborators = new ArrayList<>();
 
   @ManyToMany(mappedBy = "roles")
